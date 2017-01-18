dab8147a75e5057210837edf1452d85e52123681
Saugat Rai <Saugat Rai>
2017-01-18 07:13:50 +0545

    Custom Commit

---
+++ app/src/main/java/com/example/rai/customlistview/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.rai.customlistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView l;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/example/rai/customlistview/TestAdapter.java
@@ -0,0 +1,30 @@
+package com.example.rai.customlistview;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by sangitashres on 1/3/2017.
+ */
+public class TestAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
